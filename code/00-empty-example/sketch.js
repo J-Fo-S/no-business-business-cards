7259e77f1194bda591e3dfa254ef823dd6e3fe68
J-Fo-S <sherm_jonathan@hotmail.com>
2017-12-15 15:51:44 +0800

    no message

--- code/00-empty-example/sketch.js
+++ code/00-empty-example/sketch.js
@@ -1,10 +1,12 @@
 
-
+// "sets up" our canvas - it only happens ONCE
 function setup() {
-	createCanvas(640, 480); //width and height of my canvas
-	
+	createCanvas(x, y); //width and height IN PIXELS of my canvas
+	ellipse(x/2, y/2, 100, 100); // args are x, y, w, h
+
 }
 
+// This "draws" our code - it happens REPEATEDLY
 function draw() {
 
 }
