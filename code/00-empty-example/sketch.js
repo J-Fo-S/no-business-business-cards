0d82f2c88a6a8f44129164d5a62e5d6b2c22e906
J-Fo-S <sherm_jonathan@hotmail.com>
2018-03-01 14:42:09 +0800

    Update sketch.js

--- code/00-empty-example/sketch.js
+++ code/00-empty-example/sketch.js
@@ -1,8 +1,7 @@
 
 // "sets up" our canvas - it only happens ONCE
 function setup() {
-	createCanvas(x, y); //width and height IN PIXELS of my canvas
-	ellipse(x/2, y/2, 100, 100); // args are x, y, w, h
+	
 
 }
 
